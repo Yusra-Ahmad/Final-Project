188b9fa2f04dbbfe13d5685b6052898ae78b75ee
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-04-19 14:18:29 +0200

    cahnged logo and background of navbar and more changes to make it more responsive

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,9 +1,14 @@
 import "./header.scss";
 import Navbar from "../navbar/Navbar";
+import logo from "../../assets/bliss3.svg";
 const Hero = () => {
   return (
     <div className="hero">
-      <Navbar />
+      <div>
+        <Navbar />
+      </div>
+      <img src={logo} alt="" className="logo" />
+      {/* <video src={logo} className="logo" autoPlay loop muted></video> */}
     </div>
   );
 };
