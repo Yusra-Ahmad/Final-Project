3768a8e89a40ffc528ebb2fb5a72e70afa2ed158
sachin-patil07 <sachin.patil@dci-student.org>
2024-04-25 00:20:44 +0200

    registration update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Homepage from "./components/homepage/Homepage";
 import Login from "./components/login/Login";
+import Register from "./components/register/Register";
 
 function App() {
   return (
     <>
       <Homepage />
       <Login />
+      <Register />
     </>
   );
 }
