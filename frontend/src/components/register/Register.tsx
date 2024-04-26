21f60a050db61a9e80f4813394c402c4597b4504
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-04-26 14:10:24 +0200

    modified User.js and Register.tsx

--- frontend/src/components/register/Register.tsx
+++ frontend/src/components/register/Register.tsx
@@ -1,71 +1,99 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./register.scss";
 
 const Register = () => {
-  const data = { name: "", email: "", password: "" };
-  const [inputData, setInputData] = useState(data);
+  const firstnameInput = useRef<HTMLInputElement>(null);
+  const lastnameInput = useRef<HTMLInputElement>(null);
+  const emailInput = useRef<HTMLInputElement>(null);
+  const passwordInput = useRef<HTMLInputElement>(null);
+  const confirmPasswordInput = useRef<HTMLInputElement>(null);
+
   const [register, setRegister] = useState(false);
-  useEffect(() => {
-    console.log("Register");
-  }, [register]);
-  function handleData(e) {
-    setInputData({ ...inputData, [e.target.name]: e.target.value });
-    console.log(inputData);
-  }
-  function handleSubmit(e) {
+  // useEffect(() => {}, [register]);
+  // function handleData(e) {
+  //   setInputData({ ...inputData, [e.target.name]: e.target.value });
+  //   console.log("inputData", inputData);
+  // }
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!inputData.name || !inputData.email || !inputData.password) {
-      alert("All fields are Mandatory");
-    } else {
-      setRegister(true);
+    // if (
+    //   !firstnameInput ||
+    //   !lastnameInput ||
+    //   !emailInput ||
+    //   !passwordInput ||
+    //   !confirmPasswordInput
+    // ) {
+    //   alert("All fields are Mandatory");
+    // }
+    const email = emailInput.current.value.trim();
+    const firstname = firstnameInput.current.value.trim();
+    const lastname = lastnameInput.current.value.trim();
+    const password = passwordInput.current.value.trim();
+    const confirmPassword = confirmPasswordInput.current.value.trim();
+
+    try {
+      const config = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          firstname,
+          lastname,
+          password,
+          confirmPassword,
+        }),
+      };
+      const request = await fetch(
+        "http://localhost:3020/auth/register",
+        config
+      );
+      const result = await request.json();
+      console.log("result", result);
+    } catch (error) {
+      console.log(error.message);
     }
-  }
+  };
   return (
     <div className="register-container">
-      <pre>
+      {/* <pre>
         {" "}
         {register ? (
           <h2>Hello {inputData.name}, you have Registered successfully</h2>
         ) : (
           ""
         )}{" "}
-      </pre>
+      </pre> */}
       <form className="register-form" onSubmit={handleSubmit}>
         <p>Registration </p>
         <input
           type="text"
           placeholder="First Name*"
           name="name"
-          value={inputData.name}
-          onChange={handleData}
+          ref={firstnameInput}
         />
         <input
           type="text"
           placeholder="Last Name*"
           name="name"
-          value={inputData.name}
-          onChange={handleData}
+          ref={lastnameInput}
         />
         <input
           type="email"
           placeholder="Email*"
           name="email"
-          value={inputData.email}
-          onChange={handleData}
+          ref={emailInput}
         />
         <input
-          type="password"
+          type="text"
           placeholder="Password*"
           name="password"
-          value={inputData.password}
-          onChange={handleData}
+          ref={passwordInput}
         />
         <input
-          type="password"
+          type="text"
           placeholder="Confirm Password*"
           name="password"
-          value={inputData.password}
-          onChange={handleData}
+          ref={confirmPasswordInput}
         />
         <button type="submit">Submit</button>
       </form>
