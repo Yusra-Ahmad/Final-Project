8c9824348ffa840aef89741e7a749f8b97f75fa4
Hetal-Ahir <ahirhetal2409@gmail.com>
2024-05-08 15:53:09 +0200

    modified Header.tsx and header.scss for animation effect and contact.tsx and contact.scss Integrated form for mail connection

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,24 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
-// import "./header.scss";
+import { useEffect, useRef, useState, WheelEvent } from "react";
+import "./header.scss";
 import Navbar from "../navbar/Navbar";
-import img from "../../assets/hero206.jpg";
-import img1 from "../../assets/1.jpg";
-import img2 from "../../assets/2.jpg";
-import img3 from "../../assets/3.jpg";
-import img4 from "../../assets/4.jpg";
+import logo from "../../assets/logo.svg";
 
-const Hero: React.FC = () => {
+const App = () => {
   const pagesRef = useRef<HTMLDivElement[]>([]);
   const currentPageRef = useRef<number>(0);
   const isTransitioningRef = useRef<boolean>(false);
-  const blankRef = useRef<HTMLDivElement>(null);
+  const [wheelEnabled, setWheelEnabled] = useState(true);
 
   useEffect(() => {
     showInitialPage();
-    return () => {
-      pagesRef.current = [];
-    };
-    // createBlank();
   }, []);
 
   const showInitialPage = () => {
@@ -28,26 +20,8 @@ const Hero: React.FC = () => {
     });
     pagesRef.current[currentPageRef.current].classList.add("active");
     pagesRef.current[currentPageRef.current].classList.remove("inactive");
-    console.log(pagesRef);
+    console.log("pagesRef:", pagesRef);
   };
-
-  // const createBlank = () => {
-  //   const blank = document.createElement("div");
-  //   blank.style.backgroundColor = window
-  //     .getComputedStyle(document.body, null)
-  //     .getPropertyValue("background-color");
-  //   blank.id = "blank";
-
-  //   blankRef.current?.appendChild(blank);
-  // };
-
-  // const showBlank = () => {
-  //   blankRef.current?.classList.add("visible");
-  // };
-
-  // const hideBlank = () => {
-  //   blankRef.current?.classList.remove("visible");
-  // };
 
   const checkLast = (direction: number) => {
     if (
@@ -65,80 +39,126 @@ const Hero: React.FC = () => {
   };
 
   const scrollToSection = (direction: number) => {
-    const easeInTimer = 1000;
+    console.log("Scrolling to section activated");
+    setTimeout(() => {
+      setWheelEnabled(() => true);
+    }, 1000); // after 2 second (can be changed), enable the scroll again
     if (isTransitioningRef.current) return;
     isTransitioningRef.current = true;
     if (checkLast(direction)) {
       return;
     }
-    // why? come back to this
-    showInitialPage();
+
     pagesRef.current[currentPageRef.current].style.transform = `translateY(${
       direction * -100
     }vh)`;
-    pagesRef.current[
-      currentPageRef.current
-    ].style.transition = `transform ease-out`;
-    let previousPage = currentPageRef.current;
+    // pagesRef.current[
+    //   currentPageRef.current
+    // ].style.transition = `transform ease-out 2s`;
+    const previousPage = currentPageRef.current;
+
     currentPageRef.current =
       (currentPageRef.current + pagesRef.current.length + direction) %
       pagesRef.current.length;
+    console.log("previousPage", previousPage);
+    console.log("currPageRef.current", currentPageRef.current);
 
-    // setTimeout(() => {
-    //   pagesRef.current[previousPage].classList.remove("active");
-    //   pagesRef.current[previousPage].classList.add("inactive");
-    //   pagesRef.current[currentPageRef.current].classList.add("active");
-    //   pagesRef.current[currentPageRef.current].classList.remove("inactive");
-    //   pagesRef.current[currentPageRef.current].style.transition = "";
-    //   // showBlank();
-    // }, (easeInTimer * 3) / 4);
+    pagesRef.current[previousPage].classList.remove("active");
+    pagesRef.current[previousPage].classList.add("inactive");
+    pagesRef.current[currentPageRef.current].classList.add("active");
+    pagesRef.current[currentPageRef.current].classList.remove("inactive");
+    pagesRef.current[currentPageRef.current].style.transform = "translateY(0)";
 
-    // setTimeout(() => {
-    //   // hideBlank();
-    // }, easeInTimer * 2);
-
-    // setTimeout(() => {
-    //   isTransitioningRef.current = false;
-    // }, 1500);
+    isTransitioningRef.current = false;
   };
 
-  const handleWheel = (event: React.WheelEvent<HTMLDivElement>) => {
-    const direction = Math.sign(event.deltaY);
+  const handleWheel = (event: WheelEvent<HTMLDivElement>) => {
+    const direction = Math.sign(event.deltaY); // either 1 or -1 depending on scroll direction
+    setWheelEnabled(() => false); // cant run scroll multiple times
     scrollToSection(direction);
   };
 
   return (
-    <div className="hero" onWheel={handleWheel}>
+    <div className="hero" onWheel={wheelEnabled ? handleWheel : undefined}>
       <Navbar />
-      {/* make el unique */}
-      <div className="page" ref={(el) => el && pagesRef.current.push(el)}>
+
+      <div
+        className="page page1"
+        style={{
+          transform: "translateY(0px)",
+          transition: "transform ease-out 1s",
+        }}
+        ref={(el: HTMLDivElement): void => {
+          if (pagesRef.current.length < 1) {
+            console.log("this");
+            el && pagesRef.current.push(el);
+          }
+        }}
+      >
         <h2>Page 1</h2>
         <p>This is some text overlaid on the first image.</p>
-        <img src={img} alt="" />
       </div>
-      <div className="page" ref={(el) => el && pagesRef.current.push(el)}>
+      <div
+        style={{
+          transform: "translateY(100vh)",
+          transition: "transform ease-out 1s",
+        }}
+        className="page page2"
+        ref={(el: HTMLDivElement): void => {
+          if (pagesRef.current.length < 2) {
+            el && pagesRef.current.push(el);
+          }
+        }}
+      >
         <h2>Page 2</h2>
         <p>This is some text overlaid on the second image.</p>
-        <img src={img1} alt="" />
       </div>
-      <div className="page" ref={(el) => el && pagesRef.current.push(el)}>
+      <div
+        style={{
+          transform: "translateY(100vh)",
+          transition: "transform ease-out 1s",
+        }}
+        className="page page3"
+        ref={(el: HTMLDivElement): void => {
+          if (pagesRef.current.length < 3) {
+            el && pagesRef.current.push(el);
+          }
+        }}
+      >
         <h2>Page 3</h2>
         <p>This is some text overlaid on the third image.</p>
-        <img src={img2} alt="" />
       </div>
-      <div className="page" ref={(el) => el && pagesRef.current.push(el)}>
+      <div
+        style={{
+          transform: "translateY(100vh)",
+          transition: "transform ease-out 1s",
+        }}
+        className="page page4"
+        ref={(el: HTMLDivElement): void => {
+          if (pagesRef.current.length < 4) {
+            el && pagesRef.current.push(el);
+          }
+        }}
+      >
         <h2>Page 4</h2>
         <p>This is some text overlaid on the fourth image.</p>
-        <img src={img3} alt="" />
       </div>
-      <div className="page" ref={(el) => el && pagesRef.current.push(el)}>
+      <div
+        style={{
+          transform: "translateY(100vh)",
+          transition: "transform ease-out 1s",
+        }}
+        className="page page5"
+        ref={(el: HTMLDivElement): void => {
+          if (pagesRef.current.length < 5) {
+            el && pagesRef.current.push(el);
+          }
+        }}
+      >
         <h2>Page 5</h2>
         <p>This is some text overlaid on the fifth image.</p>
-        <img src={img4} alt="" />
       </div>
-      <div ref={blankRef}></div>
     </div>
   );
 };
-
-export default Hero;
+export default App;
