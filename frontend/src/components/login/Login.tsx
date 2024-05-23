1b7d8e2a787ea77e0c3182043a94a6949f3f8662
Hetal Ahir <136313501+Hetal2409@users.noreply.github.com>
2024-05-23 11:50:17 +0200

    Homepage hetal (#39)
    * added user icon in navbar
    
    * Implemented state management for the user icon in the navbar.tsx. Applied corresponding styles in navbar.scss.
    
    * modified Header.tsx and header.scss for animation effect and contact.tsx and contact.scss Integrated form for mail connection
    
    * modified package.json
    
    * created and modified about.tsx and about.scss
    
    * modified About.tsx
    
    * Improving nav responsiveness and homepage design
    
    * modified request.http
    
    * modified Login.tsx and login.scss added error message div
    
    * modified package.json ( added one missing coma)
    
    * modified request.http, User.js ,authRouter.js, Login.tsx, login.scss
    	new file validatePassword.js
    
    * modified Menu.tsx

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -40,7 +40,6 @@ const Login = () => {
     }
   }, [token, navigate, location.state]);
 
-
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const email = emailInput.current?.value.trim() || "";
@@ -91,7 +90,11 @@ const Login = () => {
           </p>
         )}
 
-        {!token && location?.state?.from === "/service" && <p style={{ color: " #eccd7c", fontSize: "22px" }}>Login to Book an appointment. </p>}
+        {!token && location?.state?.from === "/service" && (
+          <p style={{ color: " #eccd7c", fontSize: "22px" }}>
+            Login to Book an appointment.{" "}
+          </p>
+        )}
         <div className="input-div">
           <input
             ref={emailInput}
@@ -120,15 +123,21 @@ const Login = () => {
             {showPassword ? <FaEye /> : <FaEyeSlash />}
           </span>
         </div>
+        <div>
+          <Link className="forgot-password" to="">
+            Forgot password ?
+          </Link>
+        </div>
         <div className="login-div">
           <button type="submit" className="login-button">
             Login
           </button>
+
           <Link className="register" to="/register">
             Create an account
           </Link>
         </div>
-      </form>{" "}
+      </form>
     </div>
   );
 };
